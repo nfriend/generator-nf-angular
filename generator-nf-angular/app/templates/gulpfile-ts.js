2691288bd65fec5060ef2f06d556a2317171c9f8
Nathan Friend <contact@nathanfriend.com>
2014-05-24 21:47:02 -0500

    minor changes to gulpfile.js

--- generator-nf-angular/app/templates/gulpfile-ts.js
+++ generator-nf-angular/app/templates/gulpfile-ts.js
@@ -8,15 +8,48 @@ var connect = require('gulp-connect');
 var bowerFiles = require('gulp-bower-files');
 var filter = require('gulp-filter');
 var clean = require('gulp-clean');
+var debug = require('gulp-debug');
+var livereload = require('gulp-livereload');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+
 var merge = require('merge-stream');
 var runSequence = require('run-sequence');
 
+var server = livereload();
+
 gulp.task('default', function (callback) {
-    runSequence('clean', ['stylesheets', 'scripts', 'views', 'images', 'fonts', 'bower-components'], 'inject', ['connect', 'watchers']);
+    runSequence('devbuild', ['connect', 'watchers'], callback);
 });
+
+gulp.task('devbuild', function (callback) {
+    runSequence('clean', ['stylesheets', 'scripts', 'views', 'images', 'fonts', 'bower-components'], 'inject', callback);
+});
+
+//gulp.task('distbuild', function (callback) {
+//    runSequence('clean:dist', function () {
+//        return gulp.src('devbuild/**/*')
+//            .pipe(filter('*.js'))
+//            .pipe(uglify())
+//            .pipe(concat('application.js'))
+//            .pipe(gulp.dest('dist'))
+//            .pipe(filter.restore())
+//            .pipe(filter('*.css'))
+//            .pipe(minifyCSS())
+//            .pipe(concat('application.css'))
+//            .pipe(gulp.dest('dist'))
+//            .pipe(filter.restore())
+//    }, callback);
+//});
 
 gulp.task('clean', function () {
     return gulp.src('devbuild', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('clean:dist', function () {
+    return gulp.src('dist', { read: false })
         .pipe(clean());
 });
 
@@ -34,6 +67,13 @@ gulp.task('scripts', function () {
             .pipe(tsc())
             .pipe(gulp.dest('devbuild/scripts'))
     );
+});
+
+gulp.task('minify:scripts', function (callback) {
+    runSequence('devbuild', function () {
+        return gulp.src('devbuild/scripts/**/*.js')
+            .pipe('dist/application.js');
+    }, callback);
 });
 
 gulp.task('views', function () {
@@ -93,45 +133,41 @@ gulp.task('connect', function (callback) {
     callback();
 });
 
-gulp.task('reload', function () {
-    gulp.src('app/scripts/app.ts').pipe(connect.reload());
-});
-
 gulp.task('watchers', function (callback) {
     var scriptWatcher = gulp.watch(['app/**/*.{ts,js}', '!app/bower_components/**/*', '!app/scripts/definitions/**/*']);
     scriptWatcher.on('change', function (event) {
         console.log('Script ' + event.path + ' was ' + event.type + ', running script tasks...');
-        runSequence('scripts', 'inject', 'reload');
+        runSequence('scripts', 'inject', function () { server.changed(event.path); });
     });
 
-    var stylesheetWatcher = gulp.watch(['app/**/*.{css,less,sass,scss}', '!app/bower_components/**/*'], ['stylesheets', 'inject', 'reload']);
+    var stylesheetWatcher = gulp.watch(['app/**/*.{css,less,sass,scss}', '!app/bower_components/**/*']);
     stylesheetWatcher.on('change', function (event) {
         console.log('Stylesheet ' + event.path + ' was ' + event.type + ', running stylesheet tasks...');
-        connect.reload();
+        runSequence('stylesheets', 'inject', function () { server.changed(event.path); });
     });
 
-    var imageWatcher = gulp.watch(['app/**/*.{png,jpg,jpeg,gif,ico,svg}', '!app/bower_components/**/*'], ['images', 'reload']);
+    var imageWatcher = gulp.watch(['app/**/*.{png,jpg,jpeg,gif,ico,svg}', '!app/bower_components/**/*']);
     imageWatcher.on('change', function (event) {
         console.log('Image ' + event.path + ' was ' + event.type + ', running image tasks...');
-        connect.reload();
+        runSequence('images', function () { server.changed(event.path); });
     });
 
-    var fontWatcher = gulp.watch(['app/**/*.{woff,ttf,eot}', '!app/bower_components/**/*'], ['fonts', 'reload']);
+    var fontWatcher = gulp.watch(['app/**/*.{woff,ttf,eot}', '!app/bower_components/**/*']);
     fontWatcher.on('change', function (event) {
         console.log('Font ' + event.path + ' was ' + event.type + ', running font tasks...');
-        connect.reload();
+        runSequence('fonts', function () { server.changed(event.path); });
     });
 
-    var viewWatcher = gulp.watch(['app/**/*.{html,htm}', '!app/bower_components/**/*'], ['views', 'reload']);
+    var viewWatcher = gulp.watch(['app/**/*.{html,htm}', '!app/bower_components/**/*']);
     viewWatcher.on('change', function (event) {
         console.log('View ' + event.path + ' was ' + event.type + ', running view tasks...');
-        connect.reload();
+        runSequence('views', function () { server.changed(event.path); });
     });
 
-    var bowerComponentWatcher = gulp.watch(['app/bower_components/**/*'], ['default']);
+    var bowerComponentWatcher = gulp.watch(['app/bower_components/**/*']);
     bowerComponentWatcher.on('change', function (event) {
-        console.log('Bower component ' + event.path + ' was ' + event.type + ', running default task...');
-        connect.reload();
+        console.log('Bower component ' + event.path + ' was ' + event.type + ', starting clean by running all tasks...');
+        runSequence('devbuild', function () { server.changed(event.path); });
     });
 
     callback();
